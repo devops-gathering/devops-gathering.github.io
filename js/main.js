3542284515951942d74fd1d1678c98892c2c2884
Hugo Builder <niclas.mietz+hugobuilder@bee42.com>
2017-01-24 19:36:51 +0000

    CircleCi build #99 with git commit 8b3ad4b1085ea9a0abd6a5bb6f4bee5eb0254760

--- js/main.js
+++ js/main.js
@@ -3,14 +3,29 @@ $(document).ready(function(){
 	// Workshop and Program accordion
 	$('.accordion .acc-container').each(function(){
 		$(this).click(function(){
-			$('.acc-container').removeClass('active').children('.acc-content').slideUp();
+			if($(this).hasClass('active')) {
+				$(this).toggleClass('active');
+			}
 			$(this).children('.acc-content').slideToggle();
-			$(this).addClass('active');
+			$('.acc-container h3').children('.acc-content').slideUp();
 		});
 	});
 
-	// Show and hide navigation
+	// Add and clone navigation for resposive view
+	$('body').append('<div class="navigation-overlay"></div>');
+	$('.navigation').clone().prependTo('.navigation-overlay');
+	$('.social-media').clone().appendTo('.navigation-overlay');
+
 	$('.burger').click(function(){
 		$('.navigation-overlay').toggleClass('open');
 	});
-});	
+	
+	// Scroll To (anchor)
+	$('a').click(function(){
+		$('html, body').animate({
+			scrollTop: $( $(this).attr('href') ).offset().top - $('.navigation-container').outerHeight()
+		}, 1000);
+		$('.navigation-overlay').removeClass('open');
+		return false;
+	});	
+});
