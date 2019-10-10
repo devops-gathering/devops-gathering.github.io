02425502af634b665a445210a3ac0e01e01ceb97
Hugo Builder <niclas.mietz+hugobuilder@bee42.com>
2019-10-10 12:42:20 +0000

    GitLab CI build #42160 with git commit a446c

---
+++ js/cookieconsent-banner.js
@@ -0,0 +1,23 @@
+ window.cookieconsent.initialise({
+   "palette": {
+     "popup": {
+       "background": "#3e4f57",
+       "text": "#ffffff"
+     },
+     "button": {
+       "background": "#128abc",
+       "text": "#ffffff",
+     }
+   },
+   "theme": "edgeless",
+   "type": "opt-in",
+
+   "content": {
+     "message": "Dear visitor, we would like to use cookies in order to be able to optimally design the structure and contents of our website for you. May we? More information can be found in our",
+     "dismiss": "No rather not!",
+     "allow": "Yes, I trust you.",
+     "deny": "No rather not!",
+     "link": "Privacy Policy",
+     "href": "https://devops-gathering.io/privacy-policy/"
+   }
+ });
